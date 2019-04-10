efe59ad0d9ac13c624586242aa5feab21849372e
CatIsland123 <1289685016@qq.com>
2019-04-10 20:33:56 +0800

    第二次

---
+++ src/DemonString.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DemonString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String  s="this is a test of java";
+		int n=s.split("s").length-1 ;
+		System.out.println("S的个数为："+n);
+	
+}
+}
