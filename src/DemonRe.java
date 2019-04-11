ba9a9dc7fd46bc86a4ec21e5efdbb2b169072174
CatIsland123 <1289685016@qq.com>
2019-04-11 16:03:59 +0800

    4.(3)

--- src/DemonRe.java
+++ src/DemonRe.java
@@ -5,8 +5,11 @@ public class DemonRe {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		//第一题
        System.out.println("(0000)0000-0000".matches("\\(\\d{4}\\)\\d{4}-\\d{4}"));
-	/*
+	   /*
        String bString="故障PROBLE,服务器:181.116.222.88发生: CPU空闲5分钟内小于20%故障!";
        String regEx="((2[0-4]\\d|25[0-5]|[01]?\\d\\d?)\\.){3}(2[0-4]\\d|25[0-5]|[01]?\\d\\d?))"; 
        Pattern p = Pattern.compile(regEx); 
@@ -18,6 +21,8 @@ public class DemonRe {
        //break;   加break则提取string中的一个IP
        }
        */
+       
+       //第二题
        String str="故障PROBLE,服务器:181.116.222.88发生: CPU空闲5分钟内小于20%故障!";
        Pattern p=Pattern.compile("(\\d{1,3}\\.)\\d{3}\\.\\d{1,3}\\.\\d{2}");
        Matcher m=p.matcher(str);
@@ -25,6 +30,10 @@ public class DemonRe {
     	   String temp=m.group();
     	   System.out.println("ip为："+temp);
        }
+       //(3)
+       String str1="caaammabbbbbssssdddd";
+       String resultStr1=str1.replaceAll("(.)(\\1)*","$2");
+       System.out.println("压缩后的字符为："+resultStr1);
     
 	
 	}
