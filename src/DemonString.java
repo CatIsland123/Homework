edd8cdb2955b05f3359e4140ce2ed13a0f741f6a
CatIsland123 <1289685016@qq.com>
2019-04-11 14:58:12 +0800

    1(5)

--- src/DemonString.java
+++ src/DemonString.java
@@ -9,10 +9,13 @@ public class DemonString {
 		String  s="this is a test of java";
 		int n=s.split("s").length-1 ;
 		System.out.println("S的个数为："+n);
+		//(2)
 		String newS=s.substring(10, 14);//空格也算一个
 		System.out.println("新的test为："+newS);
+		//(3)
 		String[]str=s.split(" ");
 		System.out.println("数组为："+Arrays.toString(str ));
+		//(4)
 		StringBuffer a=new StringBuffer(s);
 		Pattern p=Pattern.compile("[a-z]+\\s*");
 		Matcher m=p.matcher(a);
@@ -21,6 +24,17 @@ public class DemonString {
 		a.replace(m.start(0), m.start()+1, temp.substring(0,1).toUpperCase());
 		}
 	    System.out.println("首字母大写的新字符串："+a);
+	    
+	   //(5)
+	    StringBuffer b=new StringBuffer(s);
+	    System.out.println("第一种倒序："+b.reverse());
+	     char  [] b1=s.toCharArray();
+	   // String resultStr = null ;
+	    StringBuffer c=new StringBuffer();
+	    for(int i=b1.length-1 ;i>=0;i--) {
+	       c.append(b1[i]);
+	    }
+	    System.out.print("第二种倒序为："+c);
 		
 }
 }
