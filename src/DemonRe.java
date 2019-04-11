3f73c365220b02e24eae61b6e5595c5bbd828ab6
CatIsland123 <1289685016@qq.com>
2019-04-11 15:58:14 +0800

    第四题的第一二小题

---
+++ src/DemonRe.java
@@ -0,0 +1,32 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DemonRe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("(0000)0000-0000".matches("\\(\\d{4}\\)\\d{4}-\\d{4}"));
+	/*
+       String bString="故障PROBLE,服务器:181.116.222.88发生: CPU空闲5分钟内小于20%故障!";
+       String regEx="((2[0-4]\\d|25[0-5]|[01]?\\d\\d?)\\.){3}(2[0-4]\\d|25[0-5]|[01]?\\d\\d?))"; 
+       Pattern p = Pattern.compile(regEx); 
+       Matcher m = p.matcher(bString); 
+
+       while (m.find()) {
+       String result=m.group();
+       System.out.println(result);
+       //break;   加break则提取string中的一个IP
+       }
+       */
+       String str="故障PROBLE,服务器:181.116.222.88发生: CPU空闲5分钟内小于20%故障!";
+       Pattern p=Pattern.compile("(\\d{1,3}\\.)\\d{3}\\.\\d{1,3}\\.\\d{2}");
+       Matcher m=p.matcher(str);
+       while(m.find()) {
+    	   String temp=m.group();
+    	   System.out.println("ip为："+temp);
+       }
+    
+	
+	}
+
+}
